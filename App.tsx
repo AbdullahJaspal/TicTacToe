50e6a9ada4e6aceac1b6960ecaae3f553d865479
AbdullahJaspal <abdullahafzal4444@gmail.com>
2023-12-04 00:50:33 +0500

    save

--- App.tsx
+++ App.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import Splash from './src/screen/splash/splash';
 import {SafeAreaView} from 'react-native';
-import Welcome from './src/screen/welcome/welcome';
-import VsHuman from './src/screen/vsHuman/vsHuman';
-import VsComp from './src/screen/vsComp/vsComp';
 import MainNav from './src/navigation/mainNav';
 
 const App = () => {
